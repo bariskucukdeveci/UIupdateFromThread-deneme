e3b19399234cdaa2c4982f29b38141371ad21bbb
bariskucukdeveci <bariskucukdeveci@gmail.com>
2017-07-01 11:41:20 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace UIupdateFromThread
+{
+    public partial class Form1 : Form
+    {
+        public delegate void delUpdateUITextBox(string text);
+        Thread myUpdateThread;
+        //ThreadStart threadStart;
+        string formloadthreadname;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.Name = "First Thread";
+            formloadthreadname = Thread.CurrentThread.Name;
+            //threadStart = new ThreadStart();
+            myUpdateThread = new Thread(GetTheThreadStarted);
+            //todo: Timer denemesi yapılabilir...
+            //myTimer timer = new myTimer();
+            myUpdateThread.Name = "Second Thread";
+            myUpdateThread.Start();
+        }
+        private void GetTheThreadStarted()
+        {
+            delUpdateUITextBox DelUpdateUITextBox = new delUpdateUITextBox(UpdateUITextBox);
+            this.textBox1.BeginInvoke(DelUpdateUITextBox, "I was updated from: " + myUpdateThread.Name + ", not " + formloadthreadname);
+        }
+        public void UpdateUITextBox(string textBoxString)
+        {
+            this.textBox1.Text = textBoxString;
+            
+        }
+    }
+}
